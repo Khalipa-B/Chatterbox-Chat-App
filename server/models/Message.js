6c38b8b361638358bb2e3203e137d74ff39b61aa
Khalipa <khalipa.baba@gmail.com>
2025-07-22 02:01:59 +0200

    updated Message.js - corrected code

--- server/models/Message.js
+++ server/models/Message.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
   text: String,
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }, // null means public message
-  createdAt: { type: Date, default: Date.now },
   room: { type: String, default: null },
   reactions: [
     {
@@ -12,8 +11,6 @@ const messageSchema = new mongoose.Schema({
       type: String, // e.g., 'like', 'love', 'laugh', etc.
     },
   ],
-});
-});
-
+}, { timestamps: true });
 
 module.exports = mongoose.model('Message', messageSchema);
